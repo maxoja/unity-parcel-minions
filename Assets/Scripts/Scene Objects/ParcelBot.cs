808adf752332ae327a2cac68c8d8a59fe92fac4c
= <->
2018-11-04 11:47:38 +0000

    Integrate Battery Bar

--- Assets/Scripts/Scene Objects/ParcelBot.cs
+++ Assets/Scripts/Scene Objects/ParcelBot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ParcelBot : MonoBehaviour 
+public class ParcelBot : MonoBehaviour
 {
     public Transform parcelBox;
     private int id;
@@ -13,14 +13,22 @@ public class ParcelBot : MonoBehaviour
     private Vector2Int current;
     [SerializeField]
     private Vector2Int nextStep;
+    private BatteryBar battery;
 
-    private Queue<Vector3Int> nextTarget;
     [SerializeField]
     private Text idText, posText;
+    private Queue<Vector3Int> nextTarget;
+    //private Text textComp;
 
     const float steppingSpeed = 4f;
 
     private float r = 0;
+
+    private void Awake()
+    {
+        idText = GetComponentInChildren<Text>();
+        battery = GetComponentInChildren<BatteryBar>();
+    }
 
     void Start()
     {
@@ -35,7 +43,7 @@ public class ParcelBot : MonoBehaviour
         posText.text = current.x.ToString() + "-" + current.y.ToString();
     }
 
-    IEnumerator Forever () 
+    IEnumerator Forever()
     {
         yield return new WaitForSeconds(Random.Range(0f, 2f));
         while (true)
@@ -44,6 +52,11 @@ public class ParcelBot : MonoBehaviour
             //then request the new next block from BotRouter
             if (current == nextStep)
             {
+                if (current == new Vector2Int(id, 0))
+                    battery.StartCharging();
+                else
+                    battery.StopCharging();
+                
                 if (current == target)
                 {
                     if (nextTarget == null || nextTarget.Count == 0)
@@ -55,7 +68,7 @@ public class ParcelBot : MonoBehaviour
                     {
                         if (nextTarget.Count % 2 == 1)
                             yield return new WaitForSeconds(2);
-                        
+
                         Vector3Int newTarget = nextTarget.Dequeue();
                         target = new Vector2Int(newTarget.x, newTarget.y);
 
@@ -68,14 +81,18 @@ public class ParcelBot : MonoBehaviour
                     yield return StartCoroutine(BotRouter.instance.RequestNextStep(this));
                     if (nextStep == current)
                     {
-                        yield return new WaitForSeconds(Random.Range(0.1f,0.2f));
+                        yield return new WaitForSeconds(Random.Range(0.1f, 0.2f));
+                    }
+                    else
+                    {
+                        battery.DrainBattery();
                     }
                 }
             }
 
             //ajust the value of r representing its progress toward the block
             //the value in r will be in the range of 0.0 to 1.0
-            r += Time.deltaTime*steppingSpeed;
+            r += Time.deltaTime * steppingSpeed;
 
             Vector2 updatedPosition;
             if (r >= 1)
@@ -99,7 +116,7 @@ public class ParcelBot : MonoBehaviour
             transform.position = updatedPosition;
             yield return null;
         }
-	}
+    }
 
     //this will be call after a bot was created
     public void Initialize(int id)
@@ -118,13 +135,13 @@ public class ParcelBot : MonoBehaviour
         r = 0;
 
         //assign its proper starting position
-        transform.position = new Vector2(current.x,current.y) * BoardData.gridScale;
+        transform.position = new Vector2(current.x, current.y) * BoardData.gridScale;
     }
 
     //call this function to make a bot move to a point on the board
     public void AssignNextParcelPoint(Vector2Int assignment, int holeId)
     {
-        nextTarget.Enqueue(new Vector3Int(assignment.x,assignment.y,holeId));
+        nextTarget.Enqueue(new Vector3Int(assignment.x, assignment.y, holeId));
         nextTarget.Enqueue(new Vector3Int(this.id, 0, 0));
         idText.text = holeId.ToString();
     }
@@ -136,7 +153,7 @@ public class ParcelBot : MonoBehaviour
 
     public bool IsFree()
     {
-        if (nextStep == current && current == new Vector2Int(id, 0))
+        if (nextStep == current && current == new Vector2Int(id, 0) && battery.LowBattery() == false)
             return true;
         return false;
     }
@@ -168,7 +185,7 @@ public class ParcelBot : MonoBehaviour
 
     public bool IsSendingParcel()
     {
-        if(target == new Vector2Int(id,0))
+        if (target == new Vector2Int(id, 0))
             return false;
         return true;
     }
