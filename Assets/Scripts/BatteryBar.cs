2091a9fa99b650ec6a54086b7b886236665d976c
= <->
2018-11-04 11:35:05 +0000

    Attach Battery Bar

---
+++ Assets/Scripts/BatteryBar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryBar : MonoBehaviour 
+{
+    private Image imgComp;
+    [SerializeField]
+    private int currentBattery;
+    [SerializeField]
+    private bool charging;
+
+
+    private const int maxBattery = 100;
+    private const int chargeRate = 1;
+    private const int lowBatteryThreshold = 40;
+
+	void Awake () 
+    {
+        imgComp = GetComponent<Image>();
+        currentBattery = maxBattery;
+        charging = true;
+        UpdateGraphic();
+
+        InvokeRepeating("ChargeIfInPosition", 1, 1);
+	}
+
+    void UpdateGraphic()
+    {
+        imgComp.fillAmount = (float)currentBattery / maxBattery;
+    }
+	
+    public void StartCharging(){
+        charging = true;
+    }
+
+    public void StopCharging(){
+        charging = false;
+    }
+
+    public void DrainBattery(){
+        currentBattery -= 1;
+        UpdateGraphic();
+    }
+
+    public bool LowBattery(){
+        return currentBattery <= lowBatteryThreshold;
+    }
+
+    private void ChargeIfInPosition()
+    {
+        if (charging && currentBattery < maxBattery)
+        {
+            currentBattery += chargeRate;
+            UpdateGraphic(); 
+        }
+           
+    }
+}
+
